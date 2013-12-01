f2b94dcbd3e83a0b36539527acfaa090cbf43d51
solygen <account@solygen.de>
2013-12-01 08:58:06 +0100

    intend

--- magickartenmarkt.de/detail-view-userscript.js
+++ magickartenmarkt.de/detail-view-userscript.js
@@ -1,26 +1,32 @@
 // ==UserScript==
-// @name	magiccardmarket.eu / magickartenmarkt.de (mkm) average card price (sold)
-// @version	0.0.1
-// @namespace	https://github.com/solygen/userscripts-and-bookmarklets
-// @repository	https://github.com/solygen/userscripts-and-bookmarklets.git
-// @include	https://www.magickartenmarkt.de/*.prod
-// @include	https://www.magiccardmarket.eu/*.prod
-// @include	https://fr.magiccardmarket.eu/*.prod
-// @include	https://es.magiccardmarket.eu/*.prod
-// @include	https://id.magiccardmarket.eu/*.prod
-// @require     https://ajax.googleapis.com/ajax/libs/jquery/1.10.2/jquery.min.js
-// @license	MIT
+// @name         MKM/MCM: add average card price (sold)
+// @description  magiccardmarket.eu and magickartenmarkt.de
+// @version      0.0.1
+// @namespace    https://github.com/solygen/userscripts-and-bookmarklets
+// @repository   https://github.com/solygen/userscripts-and-bookmarklets.git
+// @license      MIT
+// @require      https://ajax.googleapis.com/ajax/libs/jquery/1.10.2/jquery.min.js
+//
+// @include      https://www.magickartenmarkt.de/*.prod
+// @include      https://www.magiccardmarket.eu/*.prod
+// @include      https://fr.magiccardmarket.eu/*.prod
+// @include      https://es.magiccardmarket.eu/*.prod
+// @include      https://id.magiccardmarket.eu/*.prod
+//
+// @updateURL    https://rawgithub.com/solygen/userscripts-and-bookmarklets/master/magickartenmarkt.de/detail-view-bookmarklet.js
+// @downloadURL  https://rawgithub.com/solygen/userscripts-and-bookmarklets/master/magickartenmarkt.de/detail-view-bookmarklet.js
+// @homepage     https://rawgithub.com/solygen/userscripts-and-bookmarklets/master/magickartenmarkt.de/detail-view-bookmarklet.js
 
 // ==/UserScript==
 
 //get chart node
 var tmp = $($.parseHTML($('embed').attr('flashvars').split('dataXML=').splice(1,1)[0])).find('line');
-        
+
 //extract data
 var start = tmp.attr('startvalue'),
     end = tmp.attr('endvalue'),
     mid = String(parseFloat((start + end)).toFixed(2) + '  €').replace('.', ',');
-        
+
 //remoe old one
 $('.custom').remove();
 //add data to dom
--- magickartenmarkt.de/metacard-userscript.js
+++ magickartenmarkt.de/metacard-userscript.js
@@ -1,16 +1,23 @@
 // ==UserScript==
-// @name	magiccardmarket/magickartenmarkt (mkm) skip meta card page
-// @version	0.0.1
-// @namespace	https://github.com/solygen/userscripts-and-bookmarklets
-// @repository	https://github.com/solygen/userscripts-and-bookmarklets.git
-// @include	https://www.magickartenmarkt.de/?mainPage=showMetacard&idMetacard=*
-// @include	https://www.magiccardmarket.eu/?mainPage=showMetacard&idMetacard=*
-// @include	https://fr.magiccardmarket.eu/?mainPage=showMetacard&idMetacard=*
-// @include	https://es.magiccardmarket.eu/?mainPage=showMetacard&idMetacard=*
-// @include	https://id.magiccardmarket.eu/?mainPage=showMetacard&idMetacard=*
-// @require     https://ajax.googleapis.com/ajax/libs/jquery/1.10.2/jquery.min.js
-// @downloardURL https://raw.github.com/solygen/userscripts-and-bookmarklets/master/magickartenmarkt.de/metacard-userscript.js
-// @license	MIT
+// @name         MKM/MCM: skip meta card page
+// @description  magiccardmarket.eu and magickartenmarkt.de
+// @version      0.0.1
+// @namespace    https://github.com/solygen/userscripts-and-bookmarklets
+// @repository   https://github.com/solygen/userscripts-and-bookmarklets.git
+// @license      MIT
+// @require      https://ajax.googleapis.com/ajax/libs/jquery/1.10.2/jquery.min.js
+
+// @include      https://www.magickartenmarkt.de/?mainPage=showMetacard&idMetacard=*
+// @include      https://www.magiccardmarket.eu/?mainPage=showMetacard&idMetacard=*
+// @include      https://fr.magiccardmarket.eu/?mainPage=showMetacard&idMetacard=*
+// @include      https://es.magiccardmarket.eu/?mainPage=showMetacard&idMetacard=*
+// @include      https://id.magiccardmarket.eu/?mainPage=showMetacard&idMetacard=*
+
+// @updateURL    https://raw.github.com/solygen/userscripts-and-bookmarklets/master/magickartenmarkt.de/metacard-bookmarklet.js
+// @downloadURL  https://raw.github.com/solygen/userscripts-and-bookmarklets/master/magickartenmarkt.de/metacard-bookmarklet.js
+// @homepage     https://github.com/solygen/userscripts-and-bookmarklets
+
+>>>>>>> intend
 
 // ==/UserScript==
 
