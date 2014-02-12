3b099e35c74ef12104638378dfcfab95fb3f2768
solygen <frank.paczynski@solygen.de>
2014-02-12 07:03:44 +0100

    add logger util

--- scripts-min/open-xchange.com/appsuite-min.user.js
+++ scripts-min/open-xchange.com/appsuite-min.user.js
@@ -1,11 +1,11 @@
 // ==UserScript==
 // @name         appsuite: age
-// @version      0.0.1
+// @version      0.0.2
 // @icon         https://raw2.github.com/solygen/userscripts/master/doc/icon/icon_032.png
 // @namespace    https://github.com/solygen/userscripts
 // @repository   https://github.com/solygen/userscripts.git
 // @license      MIT
-// 
+//
 // @include      http://ox/appsuite/*
 // @include      https://appsuite-dev.open-xchange.com*
 // @include      https://appsuite-sprint.open-xchange.com*
@@ -16,7 +16,8 @@
 // @downloadURL  https://rawgithub.com/solygen/userscripts/master/scripts-min/open-xchange.com/appsuite-min.user.js
 // @homepage     https://github.com/solygen/userscripts
 // ==/UserScript==
-!function(){"use strict";function a(a){for(var b=["sec","min","hour","day","week","month","year","decade"],c=["60","60","24","7","4.35","12","10"],d=new Date,e=(d-a)/1e3-3600,f=0;e>=c[f]&&f<c.length-1;f++)e/=c[f];return e=Math.round(e),1!==e&&(b[f]+="s"),e+" "+b[f]}var b=setInterval(function(){var c=new Date,d=$($(".launchers").find("li")[0]).clone();return d.length?(//set date by ox version
+!function(){"use strict";function a(a){for(var b=["sec","min","hour","day","week","month","year","decade"],c=["60","60","24","7","4.35","12","10"],d=new Date,e=(d-a)/1e3-3600,f=0;e>=c[f]&&f<c.length-1;f++)e/=c[f];return e=Math.round(e),1!==e&&(b[f]+="s"),e+" "+b[f]}//logger
+unsafeWindow.ox.log={add:function(a,b){b=b||"general",unsafeWindow.ox.logdata=unsafeWindow.ox.logdata||{},unsafeWindow.ox.logdata[b]=unsafeWindow.ox.logdata[b]||[],unsafeWindow.ox.logdata[b].push(a)},show:function(a){unsafeWindow.ox.logdata=unsafeWindow.ox.logdata||{},console.log(JSON.stringify(unsafeWindow.ox.logdata[a]||[],void 0,2))},clear:function(a){unsafeWindow.ox.logdata=unsafeWindow.ox.logdata||{},a?unsafeWindow.ox.logdata[a]=void 0:unsafeWindow.ox.logdata=void 0}};var b=setInterval(function(){var c=new Date,d=$($(".launchers").find("li")[0]).clone();return d.length?(//set date by ox version
 c.setYear(ox.version.substr(-15,4)),c.setMonth(ox.version.substr(-11,2)-1),c.setDate(ox.version.substr(-9,2)),c.setHours(ox.version.substr(-6,2)),c.setMinutes(ox.version.substr(-4,2)),c.setSeconds(ox.version.substr(-2,2)),//append to DOM
 /* jshint -W107 */
 d.find("a").text(a(c)).attr("href",'javascript: ox.cache.clear().done(function() { console.log("cache clear"); })').css("color","rgb(255, 122, 0)"),/* jshint +W107 */
--- scripts/open-xchange.com/appsuite.user.js
+++ scripts/open-xchange.com/appsuite.user.js
@@ -1,11 +1,11 @@
 // ==UserScript==
 // @name         appsuite: age
-// @version      0.0.1
+// @version      0.0.2
 // @icon         https://raw2.github.com/solygen/userscripts/master/doc/icon/icon_032.png
 // @namespace    https://github.com/solygen/userscripts
 // @repository   https://github.com/solygen/userscripts.git
 // @license      MIT
-// 
+//
 // @include      http://ox/appsuite/*
 // @include      https://appsuite-dev.open-xchange.com*
 // @include      https://appsuite-sprint.open-xchange.com*
@@ -20,34 +20,57 @@
 (function () {
 
     'use strict';
-    
+
+    //logger
+    unsafeWindow.ox.log = {
+        add: function (value, key) {
+            key = key || 'general';
+            unsafeWindow.ox.logdata = unsafeWindow.ox.logdata || {};
+            unsafeWindow.ox.logdata[key] =  unsafeWindow.ox.logdata[key] || [];
+            unsafeWindow.ox.logdata[key].push(value);
+            //unsafeWindow.ox.logdata['root'].push(value + ' [' + key + ']');
+        },
+        show: function (id) {
+            unsafeWindow.ox.logdata = unsafeWindow.ox.logdata || {};
+            console.log(JSON.stringify(unsafeWindow.ox.logdata[id] || [], undefined, 2));
+        },
+        clear: function (id) {
+            unsafeWindow.ox.logdata = unsafeWindow.ox.logdata || {};
+            if (id) {
+                unsafeWindow.ox.logdata[id] = undefined;
+            } else {
+                unsafeWindow.ox.logdata = undefined;
+            }
+        }
+    };
+
     function ago(time) {
         var periods = ['sec', 'min', 'hour', 'day', 'week', 'month', 'year', 'decade'],
             lengths = ['60','60','24','7','4.35','12','10'],
             now = new Date(),
             difference = ((now - time) / 1000) - 3600;
-        
+
         for(var j = 0; difference >= lengths[j] && j < lengths.length-1; j++) {
             difference /= lengths[j];
         }
-        
+
         difference = Math.round(difference);
-        
+
         if(difference !== 1) {
             periods[j] += "s";
         }
-        
+
         return difference + ' ' + periods[j];
     }
-    
+
     var id = setInterval(function () {
 
         var da = new Date(),
             node = $($('.launchers').find('li')[0]).clone();
-        
+
         if (!node.length) return false;
-        
-        
+
+
         //set date by ox version
         da.setYear(ox.version.substr(-15, 4));
         da.setMonth((ox.version.substr(-11, 2)) -1);
@@ -55,7 +78,7 @@
         da.setHours(ox.version.substr(-6, 2));
         da.setMinutes(ox.version.substr(-4, 2));
         da.setSeconds(ox.version.substr(-2, 2));
-        
+
         //append to DOM
         /* jshint -W107 */
         node.find('a').text(ago(da))
