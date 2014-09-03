f1e4415ec2e24f17024def5a0e554bffe5089add
solygen <frank.paczynski@solygen.de>
2014-09-03 08:05:06 +0200

    kleinanzeigen: increase version number

--- scripts/kleinanzeigen.ebay.de/hide-ads.user.js
+++ scripts/kleinanzeigen.ebay.de/hide-ads.user.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @name         hide ads and premium
 // @description  kleinanzeigen.ebay.de
-// @version      0.0.1
+// @version      0.0.2
 // @icon         http://www.google.com/s2/favicons?domain=kleinanzeigen.ebay.de
 // @namespace    https://github.com/solygen/userscripts
 // @repository   https://github.com/solygen/userscripts.git
