9ffb8447560f1d565c271159a892212ed8eacbca
solygen <frank.paczynski@solygen.de>
2014-12-26 12:36:18 +0100

    move script

--- scripts/steamdb.info/sales.user.js
+++ scripts/steamdb.info/sales.user.js
@@ -1,6 +1,6 @@
 // ==UserScript==
-// @name steamdb
-// @description steamdb
+// @name steamdb.info/sales
+// @description filter irrelevant entries
 // @version 0.0.1
 // @icon http://www.google.com/s2/favicons?domain=www.steamdb.info
 // @namespace https://github.com/solygen/userscripts
@@ -15,33 +15,32 @@
 //
 // ==/UserScript==
 (function () {
-    
+
     'use strict';
-    
+
     $('.panel-ownership').hide();
-    
     $($('.motd')[1]).hide();
     $($('.motd')[0]).hide();
-    
+
     $('tr.app').each(function (index, row) {
         row = $(row);
-        
+
         var score = $(row.find('td')[6]),
             scorevalue = parseInt(score.attr('data-sort')),
             discount = row.find('td.price-discount, td.price-discount-minor'),
             discountvalue = parseInt(discount.attr('data-sort')),
             price = row.find('td.price-final'),
             pricevalue = parseInt(price.attr('data-sort'));
-        
-        // remove 
+
+        // remove
         if (scorevalue < 75 || pricevalue > 500 || discountvalue < 75) {
             row.hide();
         }
     })
-    
+
     // update badge
     $('.badge').text(
         $('tr.app:visible').length
     );
-    
+
 }) ();
