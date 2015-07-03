4f7d4af7a1b9a53a2669f53f751efa8bdf5ede4d
solygen <frank.paczynski@solygen.de>
2015-07-03 14:49:11 +0200

    new script

---
+++ scripts/magiccards.info/plain-list.user.js
@@ -0,0 +1,31 @@
+// ==UserScript==
+// @name         view as list: just show plain text
+// @description  magiccards.info
+// @version      1.0.0
+// @grant        none
+// @icon         http://magiccards.info/images/mini.gif
+// @namespace    https://github.com/solygen/userscripts
+// @repository   https://github.com/solygen/userscripts.git
+// @license      MIT
+//
+// @include      http://magiccards.info/query?q=*&v=olist*
+//
+// @updateURL    https://rawgithub.com/solygen/userscripts/master/scripts/magiccards.info/plain-list.user.js
+// @downloadURL  https://rawgithub.com/solygen/userscripts/master/scripts/magiccards.info/plain-list.user.js
+// @homepage     https://github.com/solygen/userscripts
+//
+// ==/UserScript==
+
+(function () {
+
+    'use strict';
+
+    var table = document.querySelectorAll('table')[3],
+        links = table.querySelectorAll('a'),
+        list = [];
+    Array.prototype.forEach.call(links, function (node) {
+        list.push(node.textContent);
+        console.log(node.textContent);
+    });
+    table.innerHTML = list.join("<br>");
+})();
