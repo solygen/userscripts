7cf22d4ac885457564c67d80bb170bfccbbcec30
solygen <frank.paczynski@solygen.de>
2015-11-18 20:43:50 +0100

    latest zalando layout

--- scripts/zalando.de/sortbydiscount.user.js
+++ scripts/zalando.de/sortbydiscount.user.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @name         sort by discount
 // @description  zalando.de
-// @version      1.0.1
+// @version      2.0.0
 // @grant        none
 // @icon         http://www.google.com/s2/favicons?domain=www.zalando.de
 // @namespace    https://github.com/solygen/userscripts
@@ -9,6 +9,7 @@
 // @license      MIT
 //
 // @include      http://www.zalando.de/*
+// @include      https://www.zalando.de/*
 //
 // @updateURL    https://rawgithub.com/solygen/userscripts/master/scripts/zalando.de/sortbydiscount.user.js
 // @downloadURL  https://rawgithub.com/solygen/userscripts/master/scripts/zalando.de/sortbydiscount.user.js
@@ -21,28 +22,29 @@
 
     'use strict';
 
-    var container = $('.sortBySelect');
+    var container = $('.catalogSorting_dropdown');
 
     function sortByDiscount() {
 
-        var parent = $('.productsGridList').first(),
-            nodes = parent.first().children();
+        var parent = $('.catalogArticlesList').first(),
+            nodes = parent.find('li.catalogArticlesList_item');
 
+        nodes.css('border', '1px red solid')
         nodes.sort(function (a, b) {
-            var vala = parseInt($(a).find('.salePercentFlag').text().replace('%', ''), 10),
-                valb = parseInt($(b).find('.salePercentFlag').text().replace('%', ''), 10);
+            var vala = parseInt($(a).find('.catalogArticlesList_flagDiscountRate').text().replace('%', '').replace('-', ''), 10),
+                valb = parseInt($(b).find('.catalogArticlesList_flagDiscountRate').text().replace('%', '').replace('-', ''), 10);
             if (vala < valb) {
-                return -1;
-            } else if (vala > valb) {
                 return 1;
+            } else if (vala > valb) {
+                return -1;
             }
             return 0;
         });
 
         parent.empty();
         parent.append(nodes);
-
     }
+
     if (container.length) {
         container.parent().append(
             $('<label>')
