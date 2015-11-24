4e577f5c9486d8a8e0049b1b12f429c89fa23e9b
solygen <frank.paczynski@solygen.de>
2015-11-24 12:38:06 +0100

    fix path

--- scripts/pandoc.org/expand.user.js
+++ scripts/pandoc.org/expand.user.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @name         pandoc.org: extend textarea
 // @description  pandoc.org
-// @version      1.0.0
+// @version      1.0.1
 // @grant        none
 // @icon
 // @namespace    https://github.com/solygen/userscripts
