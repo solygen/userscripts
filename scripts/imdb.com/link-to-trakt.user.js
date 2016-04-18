3accd23eeb961ac5564b4d1c1afe4743177cddb8
solygen <frank.paczynski@solygen.de>
2016-04-18 20:20:39 +0200

    fix imdb

--- scripts/imdb.com/link-to-trakt.user.js
+++ scripts/imdb.com/link-to-trakt.user.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @name         show link to trakt.tv
 // @description  imdb.com (inspired by IMDB2Trakt from Arias)
-// @version      1.0.2
+// @version      2.0.0
 // @grant        none
 // @icon         http://www.google.com/s2/favicons?domain=www.imdb.com
 // @namespace    https://github.com/solygen/userscripts
@@ -21,14 +21,15 @@
 
     'use strict';
 
-    var container = document.querySelector('#overview-top .infobar'),
+    var container = document.querySelector('.subtext'),
         separator = document.createElement('span'),
         node = document.createElement('a'),
         // uggly way of getting imdb-ID
         id = document.URL.substring(26, 35);
 
     // add separator char
-    separator.innerHTML = '&nbsp;-&nbsp;';
+    separator.innerHTML = '|';
+    separator.classList.add('ghost');
 
     // href for searching with the imdb-ID
     node.href = 'http://trakt.tv/search/imdb?query=' + id;
